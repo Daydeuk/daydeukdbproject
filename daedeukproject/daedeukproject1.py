a955b47af0b40acddc75e0d1d46112cb53349052
Daydeuk <71961156+Daydeuk@users.noreply.github.com>
2020-12-16 04:12:20 +0900

    Add files via upload

--- daedeukproject/daedeukproject1.py
+++ daedeukproject/daedeukproject1.py
@@ -1,3 +1,32 @@
 from flask import Flask, render_template, url_for
 import sqlite3
+
 app= Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('daedeuk_home.html')
+
+@app.route('/list')
+def List():
+    db= sqlite3.connect("daydeuk_list.sql")
+    db.row_factory =sqlite3.Row
+    item=db.execute('select id, cname, price, cstatus, sell_id, reservation from book_item').fetchall()
+    db.close()
+    return render_template('daedeuk_list.html', items= items)
+
+@app.route('/sell/edit/<int:book_id>/', methods=['GET', 'POST'])
+def editbook(book_id):
+    if request.method=='POST':
+        db=sqlite3.connect("daydeuk_list.db")
+        db.row_factory=sqlite3.Row
+        db.execute(
+            'update menu_item
+            'set name=?'
+            'where id=?'
+            (request.form['book_name'], book_id)
+        )
+        db.commit()
+        db.close()
+        return redirect(url_for(showMenu))
